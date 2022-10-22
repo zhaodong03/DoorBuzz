6c1aa34822e32e86960db60b7d5b9c66abf6acdf
Mo-Kzd <moseskang000@gmail.com>
2022-10-21 22:30:32 -0400

    first

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// babel.config.js
+module.exports = {
+  plugins: ["nativewind/babel"]
 };
