80ec7f421faebf7194ba4b757d88b05f4fa847dd
Mo-Kzd <moseskang000@gmail.com>
2022-10-21 22:44:00 -0400

    update babel

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-// babel.config.js
-module.exports = {
-  plugins: ["nativewind/babel"]
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: ["nativewind/babel"],
+  };
 };
